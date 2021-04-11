2733c64167bef520a03b3572510a4fc455ed4bbe
Tapas3 <75794104+Tapas3@users.noreply.github.com>
2021-04-11 19:49:20 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+       
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        console.log(this.body.speed);
+        if(this.body.speed < 3){
+        var angle=this.body.angle;
+        var position = this.body.position;
+        push();
+        translate(position.x, position.y)
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.body.width,this.body.height);
+        pop();
+        }else{
+          World.remove(world, this.body);
+          push();
+          this.Visiblity = this.Visiblity - 5;
+          pop();
+        }
+        
+      }
+}
